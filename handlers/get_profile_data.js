5b4e929e0114ff0d3ea0813d6cdd5d345b7565e8
Juan valero <juanvalero252@gmail.com>
2019-11-24 14:04:46 -0300

    add more fields for EEUU Compliance

--- handlers/get_profile_data.js
+++ handlers/get_profile_data.js
@@ -1,43 +1,43 @@
 if (!global._babelPolyfill) {
-  require('babel-polyfill')
+    require('babel-polyfill')
 }
-import { User } from '../models/user' // eslint-disable-line
+import {User} from '../models/user' // eslint-disable-line
 import pe from 'parse-error' // eslint-disable-line
 import middy from 'middy' // eslint-disable-line
-import { cors } from 'middy/middlewares' // eslint-disable-line
+import {cors} from 'middy/middlewares' // eslint-disable-line
 
 const handler = async (request, context, callback) => {
-  console.log('the infor form request ', request)
-  const [getUserError, profileData] = await to(User.get(request.email))
-  getUserError
-    ? callback(null, handleErr(getUserError, 500))
-    : callback(null, generateProfileData(profileData))
-}
+    console.log('the infor form request ', request);
+    const [getUserError, profileData] = await to(User.get(request.email));
+    getUserError
+        ? callback(null, handleErr(getUserError, 500))
+        : callback(null, generateProfileData(profileData))
+};
 
 // *** Error handling support in promises
 const to = promise =>
-  promise
-    .then(data => [null, data])
-    .catch(err => [pe(err)])
+    promise
+        .then(data => [null, data])
+        .catch(err => [pe(err)]);
 
 
 const generateProfileData = (data) => (
-  {
-    statusCode: 200,
-    body: JSON.stringify({
-      userProfile: data
-    })
-  }
-)
+    {
+        statusCode: 200,
+        body: JSON.stringify({
+            userProfile: data
+        })
+    }
+);
 
 const handleErr = (error, statusCode = 401) => {
-  console.error(' => ERROR:', error.stack)
+    console.error(' => ERROR:', error.stack);
 
-  return {
-    statusCode,
-    body: JSON.stringify({ error })
-  }
-}
+    return {
+        statusCode,
+        body: JSON.stringify({error})
+    }
+};
 
 export const getUserData = middy(handler)
-  .use(cors())
+    .use(cors());
--- handlers/updateProfile.js
+++ handlers/updateProfile.js
@@ -1,5 +1,5 @@
 if (!global._babelPolyfill) {
-  require('babel-polyfill')
+    require('babel-polyfill')
 }
 import {cors} from 'middy/middlewares'
 import {User} from '../models/user' // eslint-disable-line
@@ -7,23 +7,23 @@ import middy from 'middy' // eslint-disable-line
 
 const handler = (request, context, callback) => {
 
-  User.update(request, (err, dynamoResponse) => {
-    if (err) {
-      console.log(err)
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify('error on saving on dynamo check data schema')
-      })
-    } else {
-      console.log(dynamoResponse)
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({userCreated: true})
-      }
-      callback(null, response)
-    }
-  })
-}
+    User.update(request, (err, dynamoResponse) => {
+        if (err) {
+            console.log(err);
+            callback(null, {
+                statusCode: 500,
+                body: JSON.stringify('error on saving on dynamo check data schema')
+            })
+        } else {
+            console.log(dynamoResponse);
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({userCreated: true})
+            };
+            callback(null, response)
+        }
+    })
+};
 
 export const updateProfile = middy(handler)
-  .use(cors())
+    .use(cors());
